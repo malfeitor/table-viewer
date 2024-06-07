230faf32ffa25582768b73d3af8737d0d6ebc221
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-08 00:09:37 +0200

    feat: add page gestion in the store ✨

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -2,18 +2,27 @@ import { create } from 'zustand'
 
 export type TableStoreType = {
   displayRows: number
+  currentPage: number
   headRow: string[]
   tableRows: string[][]
   setDisplayRows: (number: number) => void
   setHeadRow: (row: string[]) => void
   setTableRows: (rows: string[][]) => void
+  setPage: (number: number) => void
+  setPreviousPage: () => void
+  setNextPage: () => void
 }
 
 export const useTableStore = create<TableStoreType>((set) => ({
   displayRows: 10,
   headRow: [],
   tableRows: [],
+  currentPage: 0,
   setDisplayRows: (number: number) => set(() => ({ displayRows: number })),
   setHeadRow: (row: string[]) => set(() => ({ headRow: row })),
   setTableRows: (rows: string[][]) => set(() => ({ tableRows: rows })),
+  setPage: (number: number) => set(() => ({ currentPage: number })),
+  setPreviousPage: () =>
+    set((state) => ({ currentPage: state.currentPage - 1 })),
+  setNextPage: () => set((state) => ({ currentPage: state.currentPage + 1 })),
 }))
