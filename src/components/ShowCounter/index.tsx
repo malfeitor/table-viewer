5d10dee1418f7b2aabbdf32ab609b97c6e17a5c5
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-08 01:51:45 +0200

    feat: show how many entries are displayed ✨

--- src/components/ShowCounter/index.tsx
+++ src/components/ShowCounter/index.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { useTableStore } from '../../utils/store'
 
 export default function ShowCounter() {
-  //   const maxEntries = useTableStore((state) => state.foundRows.length)
-  return <p>Showing 1 of 2 entries</p>
+  const maxEntries = useTableStore((state) => state.foundRows.length)
+  const displayedEntries = useTableStore((state) => state.displayedRows.length)
+  return (
+    <p>
+      Showing {displayedEntries} of {maxEntries} entries
+    </p>
+  )
 }
