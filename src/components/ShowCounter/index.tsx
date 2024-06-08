98a8f54e014e0bfad436af62564b88cbc8bfbf00
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-08 02:20:35 +0200

    feat: show min/max/total entries count ✨

--- src/components/ShowCounter/index.tsx
+++ src/components/ShowCounter/index.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { useTableStore } from '../../utils/store'
 
 export default function ShowCounter() {
+  const currentPage = useTableStore((state) => state.currentPage)
   const maxEntries = useTableStore((state) => state.foundRows.length)
   const displayedEntries = useTableStore((state) => state.displayedRows.length)
+  const displayCount = useTableStore((state) => state.displayCount)
   return (
     <p>
-      Showing 1 to {displayedEntries} of {maxEntries} entries
+      Showing {1 + displayCount * (currentPage - 1)} to{' '}
+      {displayedEntries + displayCount * (currentPage - 1)} of {maxEntries}{' '}
+      entries
     </p>
   )
 }
