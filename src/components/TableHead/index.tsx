ad72c2dd81c3754f1a4b0c852578d28ded06e139
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-10 00:44:10 +0200

    feat: display arrows to sow which column sort ✨

--- src/components/TableHead/index.tsx
+++ src/components/TableHead/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useTableStore } from '../../utils/store'
+import { FaSortUp, FaSortDown } from 'react-icons/fa'
 
 export default function TableHead() {
   const headRow = useTableStore((state) => state.headRow)
   const setSortColumnIndex = useTableStore((state) => state.setSortColumnIndex)
   const filterRows = useTableStore((state) => state.filterRows)
   const updateDisplayRows = useTableStore((state) => state.updateDisplayRows)
+  const setSortReversed = useTableStore((state) => state.setSortReversed)
+  const isSortReversed = useTableStore((state) => state.isSortReversed)
+  const sortColumnIndex = useTableStore((state) => state.sortColumnIndex)
+
+  const arrowUpActiveColor = (index: number) =>
+    isSortReversed && index === sortColumnIndex ? '000' : 'AAA'
+  const arrowDownActiveColor = (index: number) =>
+    !isSortReversed && index === sortColumnIndex ? '000' : 'AAA'
 
   const onClick = (index: number) => {
     setSortColumnIndex(index)
@@ -18,7 +27,11 @@ export default function TableHead() {
       <tr>
         {headRow.map((column: string, index: number) => (
           <th key={`table-header-${index}`} onClick={() => onClick(index)}>
-            {column}
+            {column}{' '}
+            <FaSortUp
+              style={{ position: 'absolute', color: arrowUpActiveColor(index) }}
+            />
+            <FaSortDown style={{ color: arrowDownActiveColor(index) }} />
           </th>
         ))}
       </tr>
