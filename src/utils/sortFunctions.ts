1a39a941380529fb3aa3b37b6402b3bef669ddf7
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-09 04:17:09 +0200

    fix: correct sortString comparison 🐛

--- src/utils/sortFunctions.ts
+++ src/utils/sortFunctions.ts
@@ -1,3 +1,5 @@
 export function sortString(a: string, b: string) {
-  return a.toLowerCase() < b.toLowerCase()
+  if (a.toLowerCase() < b.toLowerCase()) return -1
+  if (a.toLowerCase() > b.toLowerCase()) return 1
+  return 0
 }
