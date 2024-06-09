fd012af5e0a652e432cebf41488c6a07855698aa
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-09 22:24:49 +0200

    feat: create sort decorator who select column ✨

--- src/utils/sortFunctions.ts
+++ src/utils/sortFunctions.ts
@@ -3,3 +3,10 @@ export function sortString(a: string, b: string) {
   if (a.toLowerCase() > b.toLowerCase()) return 1
   return 0
 }
+
+export function sortDecorator(
+  sortFunction: (a: any, b: any) => number,
+  columnIndex: number
+) {
+  return (a: any, b: any) => sortFunction(a[columnIndex], b[columnIndex])
+}
