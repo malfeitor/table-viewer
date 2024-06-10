f6a58e3dfae2391d37d0c53931bcfe46021599f3
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-06-10 05:46:16 +0200

    fix: sort with LocaleLowercase by default 🐛

--- src/utils/sortFunctions.ts
+++ src/utils/sortFunctions.ts
@@ -1,6 +1,6 @@
 export function sortString(a: string, b: string) {
-  if (a.toLowerCase() < b.toLowerCase()) return -1
-  if (a.toLowerCase() > b.toLowerCase()) return 1
+  if (a.toLocaleLowerCase() < b.toLocaleLowerCase()) return -1
+  if (a.toLocaleLowerCase() > b.toLocaleLowerCase()) return 1
   return 0
 }
 
